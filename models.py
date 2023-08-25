a13f33d2f1a261b2b7e32a5d0da4367df22d2fd6
spaunder76 <arthurbousquet36@gmail.com>
2023-08-25 13:37:42 +0200

    update pep8

--- models.py
+++ models.py
@@ -2,22 +2,23 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.points = 0
-    
+
     def add_points(self, points):
         self.points += points
-    
+
     def __str__(self):
         return self.name
+
 
 class Match:
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
         self.result = None
-    
+
     def play_match(self, result):
         self.result = result
-        
+
         if result == 0.5:
             self.player1.add_points(0.5)
             self.player2.add_points(0.5)
@@ -25,10 +26,11 @@ class Match:
             self.player1.add_points(1)
         elif result == 2:
             self.player2.add_points(1)
-    
+
     def __str__(self):
         return f"{self.player1} vs {self.player2} : {self.result}"
-    
+
+
 class Tournament:
     def __init__(self, name, location, start_date, end_date, rounds=None, players=None, num_rounds=4, current_round=1, remarks=None):
         self.name = name
@@ -48,12 +50,15 @@ class Tournament:
         self.current_round += 1
 
     def __str__(self):
-        str = f"{self.name} ({self.start_date} - {self.end_date}) at {self.location}\n{len(self.players)} registered players\nCurrent round: {self.current_round}/{self.num_rounds}\nNotes: {self.remarks}\n"
+        tournament_info = (
+            f"{self.name} ({self.start_date} - {self.end_date}) at {self.location}\n"
+            f"{len(self.players)} registered players\nCurrent round: {self.current_round}/{self.num_rounds}\nNotes: {self.remarks}\n"
+        )
         for round in self.rounds:
-            str = str.append("\n{round}")
+            tournament_info += f"\n{round}"
             for match in round.matches:
-                str = str.append("\n{match}")
-        return str
+                tournament_info += f"\n{match}"
+        return tournament_info
 
 
 class Round:
@@ -71,7 +76,7 @@ class Round:
             match.play_match(result)
 
     def __str__(self):
-        res = f"Round {self.round_number} : \n"
+        round_info = f"Round {self.round_number} : \n"
         for match in self.matches:
-            res += f"{match}\n"
-        return res
+            round_info += f"{match}\n"
+        return round_info
